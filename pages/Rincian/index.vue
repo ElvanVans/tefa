de9aa627121418b16bcd0d6c1b87797981a08014
ElvanVans <142859343+ElvanVans@users.noreply.github.com>
2024-03-25 08:44:57 +0700

    Add files via upload

---
+++ pages/Rincian/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container shadow mt-5 p-5">
+    <div class="row mt-4">
+      <div class="col-4">
+        <h2>Rincian Buku</h2>
+        <img src="~/assets/img/Buku1.jpeg" class="cover" alt="Buku1" height="350px" width="300px" />
+      </div>
+      <div class="col-4 mt-5">
+        <p>Judul buku : The Psychology of money</p>
+        <p>Penulis : Morgan Housel</p>
+        <p>Penerbit : Baca</p>
+        <p>Tahun terbit : 2021</p>
+        <p>Halaman : 262 halaman</p>
+        <p>Kategori : Studi</p>
+        <p>Kelas : Ekonomi</p>
+        <p>ISBN : 978-602-6486-57-8</p>
+        <p>Rak no 4</p>
+      </div>
+    </div>
+    <div class="row">
+      <p class="mt-3">
+        Kekayaan Tidak Hanya Angka Salah satu poin utama buku ini adalah bahwa kekayaan tidak hanya tentang angka-angka atau tingkat penghasilan. Sebaliknya, Housel menekankan pentingnya perspektif jangka panjang dan pengelolaan risiko yang
+        bijaksana. Ia menggambarkan bagaimana kekayaan yang berkelanjutaN dapat dibangun melalui disiplin, kesabaran, dan kebijaksanaan.
+      </p>
+    </div>
+  </div>
+</template>
+<style scoped></style>
